da528ee572a2a085d624f8f430af49d501611ea1
Robert Shih <robertshih@google.com>
2019-11-25 22:25:42 -0800

    joyeuse: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device.mk
+++ device.mk
@@ -81,7 +81,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fastbootd
 PRODUCT_PACKAGES += \
