0465e8d05bb9ebb9100d411ee7096d116c786a71
Redick Lin <redicklin@google.com>
2020-04-20 18:28:53 +0800

    joyeuse: BOARD_CHARGER_ENABLE_SUSPEND becomes ro.charger.enable_suspend
    Define ro.charger.enable_suspend to true as charger no
    longer uses BOARD_CHARGER_ENABLE_SUSPEND.
    
    Bug: 153937817
    Change-Id: Ic76bf3a66c5e21c74e49607f54d6c0358c31bbac
    Signed-off-by: Redick Lin <redicklin@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,9 +43,6 @@ TARGET_BOOTLOADER_BOARD_NAME := joyeuse
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
 
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
 # Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
