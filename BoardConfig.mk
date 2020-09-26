e1f91f1bd0122e93f0f6901aea20df82dd87a383
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-09-26 09:18:08 +0700

    joyruse: dispendencies: change new kranuls

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := krypton-joyeuse_defconfig
+TARGET_KERNEL_CONFIG := joyeuse_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/joyeuse
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
