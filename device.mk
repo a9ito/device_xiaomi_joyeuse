06a5dfbae02d2a8655e2a445fa64dcd32f6c184a
a9ito <anggit.febriantoro@gmail.com>
2020-12-23 04:26:19 +0700

    Revert "joyeuse: Import vendor/qcom/opensource/commonsys/packages/apps/Bluetooth soong namespace"
    Co-Authored-By: Dhimas Bagus Prayoga <idontgiveashit@kry9ton.tech>

--- device.mk
+++ device.mk
@@ -249,8 +249,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/pixel \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
