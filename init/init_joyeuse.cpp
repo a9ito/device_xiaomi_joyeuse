526557852b9bb5cef3502cdf0c588303f640bdfe
a9ito <anggit.febriantoro@gmail.com>
2020-12-23 04:43:32 +0700

    joyeuse: Adapt libinit to Android 11
    [wight554: add property_override to common header]

--- init/init_joyeuse.cpp
+++ init/init_joyeuse.cpp
@@ -28,7 +28,16 @@
 #include "property_service.h"
 #include "vendor_init.h"
 
-using android::init::property_set;
+void property_override(char const prop[], char const value[], bool add)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
 
 void load_dalvik_properties() {
     char const *heapstartsize;
@@ -61,12 +70,12 @@ void load_dalvik_properties() {
         return;
     }
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
 
 std::vector<std::string> ro_props_default_source_order = {
